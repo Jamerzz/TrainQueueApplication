ecfa9c4143dc2a8603b0f647cf5415d108de3910
Eli Kapetanopoulos <kapetanopoulose@wit.edu>
2016-02-18 14:03:28 -0500

    Train and Passenger added
    Train and Passenger added

--- src/Passenger.java
+++ src/Passenger.java
@@ -2,4 +2,38 @@
  * Group 13
  */
 public class Passenger {
+
+    private String startingStation;
+    private String endingStation;
+
+    public Passenger(){
+        startingStation ="1";
+        endingStation ="2";
+    }
+
+    public Passenger(String newStart, String newEnd)
+    {
+        startingStation = newStart;
+        endingStation = newEnd;
+    }
+
+    public void setStartingStation(String newStart)
+    {
+        startingStation = newStart;
+    }
+
+    public void setEndingStation(String newEnd)
+    {
+        endingStation = newEnd;
+    }
+
+    public String getStartingStation()
+    {
+        return startingStation;
+    }
+
+    public String getEndingStation()
+    {
+        return endingStation;
+    }
 }
--- src/Train.java
+++ src/Train.java
@@ -2,4 +2,42 @@
  * Group 13
  */
 public class Train {
+
+    private Object trainRoute;
+    private String currentStation;
+    private String nextStation;
+    Passenger[] passengerArray = new Passenger[50];
+
+    public Train()
+    {
+        trainRoute = 1;
+        currentStation = "0";
+    }
+
+    public Train(Object newRoute)
+    {
+        trainRoute = newRoute;
+        currentStation = "0";
+    }
+
+    public void addPassenger(Passenger newPassenger)
+    {
+        if (newPassenger.getStartingStation() == currentStation)
+        {
+            passengerArray[numberOfPassengers()]=newPassenger;
+        }
+    }
+
+    public void removePassenger(Passenger currentPassenger)
+    {
+        if (currentPassenger.getEndingStation() == currentStation)
+        {
+
+        }
+    }
+
+    public int numberOfPassengers()
+    {
+        return passengerArray.length;
+    }
 }
